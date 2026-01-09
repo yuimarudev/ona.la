0dbbbf0871203690daf93ce032fde0f5dc463e59
yuimarudev <68449029+yuimarudev@users.noreply.github.com>
2026-01-09 12:22:57 +0900

    fix

--- src/lib/components/Embed.tsx
+++ src/lib/components/Embed.tsx
@@ -44,7 +44,7 @@ export const Embed = component$<{
       output.value = (
         <iframe
           style={{ height: "18em", width: "32em" }}
-          src={`https://www.youtube-nocookie.com/embed/${id}&amp;controls=0`}
+          src={`https://www.youtube-nocookie.com/embed/${id}?controls=0`}
           title="YouTube video player"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
